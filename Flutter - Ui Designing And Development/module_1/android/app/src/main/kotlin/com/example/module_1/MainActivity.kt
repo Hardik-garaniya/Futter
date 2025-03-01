c314f3703266bfa1d33eda886beef0f74f53f555
Hardik <hardikgaraniya32@gmail.com>
2025-03-01 12:19:29 +0530

    commit

--- Flutter - Ui Designing And Development/module_1/android/app/src/main/kotlin/com/example/module_1/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.module_1
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- Flutter - Ui Designing And Development/module_3/android/app/src/main/kotlin/com/example/module_3/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.module_3
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
